94385dd0f7443af356dfc251972c357d64c04699
theodore cleveland <ted.cleveland@gmail.com>
2023-09-21 15:47:16 +0000

    update notebook usgsmoc

---
+++ ce5364notes/_build/jupyter_execute/chapters/12usgsmocmodel/usgsmoc.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # USGS MOC Software
+# 
+# The USGS MOC software is a specialized modeling tool developed to simulate groundwater flow, solute transport, and some chemical reactions within aquifers. It is based on numerical modeling principles and has evolved over the years to address complex hydrogeological scenarios. The software employs a modular framework, allowing users (with programming expertise) to tailor it to specific applications, making it an invaluable asset for groundwater professionals and researchers.
+# 
+# 
+# :::{note}
+# I choose MOC instead of MODFLOW/MODPATH because the program is easier to understand and get working.  Accessing MODFLOW using FloPy or ModelMUSE is far more challenging than the pretty pictures on the web-a-rama suggest. You can examine how to install MODFLOW at [MODFLOW on a JupyterHub](http://54.243.252.9/ce-4363-webroot/ce4363notes/lessons/groundwatermodels/installing.pdf) and how to access using [MODFLOW using Jupyter Example(s)](http://54.243.252.9/ce-4363-webroot/ce4363notes/_build/html/lessons/groundwatermodels/15gwmodels-examples.html)
+# :::
+
+# In[ ]:
+
+
+
+
